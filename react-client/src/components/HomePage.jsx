5dbe7c8c748da0fd7438910eea941e8a99ddf1ec
ljuster <leora.juster@hyfn.com>
2018-05-06 12:55:34 +0100

    emoji picker

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
+
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        Home page
+        <Link to="">Hola superMaria, is this creepy, I'm working on building my own website from scratch
+        </Link>
+        <Picker title='Pick your emoji superMaria' emoji='point_up' />
       </div>
     )
   }
