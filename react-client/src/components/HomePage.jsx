27293b28385dd1bb45b97b5186db235b58faf16f
ljuster <leora.juster@hyfn.com>
2018-05-06 13:01:40 +0100

    socks n peanuts

--- react-client/src/components/HomePage.jsx
+++ react-client/src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import 'emoji-mart/css/emoji-mart.css'
-import { Picker } from 'emoji-mart'
+import { Picker, Emoji } from 'emoji-mart'
 
 
 class HomePage extends Component {
@@ -10,7 +10,50 @@ class HomePage extends Component {
       <div>
         <Link to="">Hola superMaria, is this creepy, I'm working on building my own website from scratch
         </Link>
-        <Picker title='Pick your emoji superMaria' emoji='point_up' />
+        <Picker title='Pick your emoji' emoji='point_up' />
+        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />        <Emoji emoji={{ id: 'peanuts', skin: 3 }} size={160} />
+        <Emoji emoji='peanuts' set='emojione' size={200} />
+        <Emoji emoji={{ id: 'socks', skin: 3 }} size={160} />
+        <Emoji emoji='socks' set='emojione' size={200} />
       </div>
     )
   }
